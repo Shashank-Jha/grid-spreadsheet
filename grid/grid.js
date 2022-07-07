fd1b6adf4a04992a0b3dff88f30a35b4da92fdd5
Shashank-Jha <53325972+Shashank-Jha@users.noreply.github.com>
2022-07-07 15:16:57 +0530

    added : Grid row and column

---
+++ grid/grid.js
@@ -0,0 +1,19 @@
+const row = 100;
+const col = 26;
+
+const rowNameContainer = document.querySelector(".row-name-container");
+const colNameContainer = document.querySelector(".column-name-container");
+
+for(let i=1;i<=row;i++){
+    const rowNameDiv = document.createElement("div");
+    rowNameDiv.setAttribute("class", "address-row");
+    rowNameDiv.innerText = i;
+    rowNameContainer.appendChild(rowNameDiv);
+}
+
+for(let i=1;i<=col;i++){
+    const colNameDiv = document.createElement("div");
+    colNameDiv.setAttribute("class", "address-col");
+    colNameDiv.innerText = i;
+    colNameContainer.appendChild(colNameDiv);
+}

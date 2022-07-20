95a60b36d875f6748728198d19d6cc31b6c5e095
Shashank-Jha <53325972+Shashank-Jha@users.noreply.github.com>
2022-07-21 02:41:14 +0530

    grid v2

--- grid/grid.js
+++ grid/grid.js
@@ -1,19 +1,32 @@
-const row = 100;
-const col = 26;
+let rows = 100;
+let cols = 26;
 
-const rowNameContainer = document.querySelector(".row-name-container");
-const colNameContainer = document.querySelector(".column-name-container");
+let addressColCont = document.querySelector(".address-col-cont");
+let addressRowCont = document.querySelector(".address-row-cont");
+let cellsCont = document.querySelector(".cells-cont");
+let addressBar = document.querySelector(".address-bar");
 
-for(let i=1;i<=row;i++){
-    const rowNameDiv = document.createElement("div");
-    rowNameDiv.setAttribute("class", "address-row");
-    rowNameDiv.innerText = i;
-    rowNameContainer.appendChild(rowNameDiv);
+for (let i = 0;i < rows;i++) {
+    let addressCol = document.createElement("div");
+    addressCol.setAttribute("class", "address-col");
+    addressCol.innerText = i+1;
+    addressColCont.appendChild(addressCol);
 }
 
-for(let i=0;i<=col;i++){
-    const colNameDiv = document.createElement("div");
-    colNameDiv.setAttribute("class", "address-col");
-    colNameDiv.innerText = String.fromCharCode(65+i);
-    colNameContainer.appendChild(colNameDiv);
+for (let i = 0;i < cols;i++) {
+    let addressRow = document.createElement("div");
+    addressRow.setAttribute("class", "address-row");
+    addressRow.innerText = String.fromCharCode(65 + i);
+    addressRowCont.appendChild(addressRow);
+}
+
+for (let i = 0;i < rows;i++) {
+    let rowCont = document.createElement("div");
+    rowCont.setAttribute("class", "row-cont");
+    for (let j = 0;j < cols;j++) {
+        let cell = document.createElement("div");
+        cell.setAttribute("class", "cell");
+        rowCont.appendChild(cell);
+    }
+    cellsCont.appendChild(rowCont);
 }
